f3abf66de765973df27e0c5034708ade430f03b9
Abraham Mughal <abmughal@cisco.com>
2024-07-08 15:09:52 -0700

    [ignore]

--- internal/provider/test_constants.go
+++ internal/provider/test_constants.go
@@ -20,18 +20,6 @@ resource "aci_l3_outside" "test" {
 }
 `
 
-const testConfigFvApMinDependencyWithFvTenant = testConfigFvTenantMin + `
-resource "aci_application_epg" "test" {
-  application_profile_dn = aci_application_profile.test.id
-  name                   = "test_epg"
-}
-
-resource "aci_application_profile" "test" {
-  tenant_dn = aci_tenant.test.id
-  name      = "test_ap"
-}
-`
-
 const testConfigFvAEPgMinDependencyWithFvAp = testConfigFvApMinDependencyWithFvTenant + `
 resource "aci_application_epg" "test" {
   application_profile_dn = aci_application_profile.test.id
@@ -50,6 +38,8 @@ resource "aci_application_epg" "test" {
   name                   = "test_epg"
 }
 `
+
+const testConfigFvApMinDependencyWithFvTenant = testConfigFvAEPgMin
 
 const testConfigL3extOutMinDependencyWithFvTenant = testConfigFvTenantMin + `
 resource "aci_vrf" "test" {
