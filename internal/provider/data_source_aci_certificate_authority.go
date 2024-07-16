CiscoDevNet/terraform-provider-aci

[minor_change] Addition of new generated resource and data source for pkiTP

internal/provider/data_source_aci_certificate_authority.go
@@ -1,0 +1,153 @@
+// Code generated by "gen/generator.go"; DO NOT EDIT.
+// In order to regenerate this file execute `go generate` from the repository root.
+// More details can be found in the [README](https://github.com/CiscoDevNet/terraform-provider-aci/blob/master/README.md).
+
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/v2/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &PkiTPDataSource{}
+
+func NewPkiTPDataSource() datasource.DataSource {
+	return &PkiTPDataSource{}
+}
+
+// PkiTPDataSource defines the data source implementation.
+type PkiTPDataSource struct {
+	client *client.Client
+}
+
+func (d *PkiTPDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	tflog.Debug(ctx, "Start metadata of datasource: aci_certificate_authority")
+	resp.TypeName = req.ProviderTypeName + "_certificate_authority"
+	tflog.Debug(ctx, "End metadata of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	tflog.Debug(ctx, "Start schema of datasource: aci_certificate_authority")
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "The certificate_authority datasource for the 'pkiTP' class",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The distinguished name (DN) of the Certificate Authority object.",
+			},
+			"parent_dn": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "The distinguished name (DN) of the parent object.",
+			},
+			"annotation": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The annotation of the Certificate Authority object.`,
+			},
+			"cert_chain": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The PEM-encoded chain of trust from the trustpoint to a trusted root authority.`,
+			},
+			"description": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The description of the Certificate Authority object.`,
+			},
+			"name": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: `The name of the Certificate Authority object.`,
+			},
+			"name_alias": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The name alias of the Certificate Authority object.`,
+			},
+			"owner_key": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The key for enabling clients to own their data for entity correlation.`,
+			},
+			"owner_tag": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `A tag for enabling clients to add their own data. For example, to indicate who created this object.`,
+			},
+			"annotations": schema.SetNestedAttribute{
+				MarkdownDescription: ``,
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"key": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The key used to uniquely identify this configuration object.`,
+						},
+						"value": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The value of the property.`,
+						},
+					},
+				},
+			},
+		},
+	}
+	tflog.Debug(ctx, "End schema of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	tflog.Debug(ctx, "Start configure of datasource: aci_certificate_authority")
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *client.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+	tflog.Debug(ctx, "End configure of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	tflog.Debug(ctx, "Start read of datasource: aci_certificate_authority")
+	var data *PkiTPResourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	setPkiTPId(ctx, data)
+
+	// Create a copy of the Id for when not found during getAndSetPkiTPAttributes
+	cachedId := data.Id.ValueString()
+
+	tflog.Debug(ctx, fmt.Sprintf("Read of datasource aci_certificate_authority with id '%s'", data.Id.ValueString()))
+
+	getAndSetPkiTPAttributes(ctx, &resp.Diagnostics, d.client, data)
+
+	if data.Id.IsNull() {
+		resp.Diagnostics.AddError(
+			"Failed to read aci_certificate_authority data source",
+			fmt.Sprintf("The aci_certificate_authority data source with id '%s' has not been found", cachedId),
+		)
+		return
+	}
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	tflog.Debug(ctx, fmt.Sprintf("End read of datasource aci_certificate_authority with id '%s'", data.Id.ValueString()))
+}

harismalk/harismalk

internal/provider/data_source_aci_certificate_authority.go
@@ -1,0 +1,174 @@
+// Code generated by "gen/generator.go"; DO NOT EDIT.
+// In order to regenerate this file execute `go generate` from the repository root.
+// More details can be found in the [README](https://github.com/CiscoDevNet/terraform-provider-aci/blob/master/README.md).
+
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ciscoecosystem/aci-go-client/v2/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &PkiTPDataSource{}
+
+func NewPkiTPDataSource() datasource.DataSource {
+	return &PkiTPDataSource{}
+}
+
+// PkiTPDataSource defines the data source implementation.
+type PkiTPDataSource struct {
+	client *client.Client
+}
+
+func (d *PkiTPDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	tflog.Debug(ctx, "Start metadata of datasource: aci_certificate_authority")
+	resp.TypeName = req.ProviderTypeName + "_certificate_authority"
+	tflog.Debug(ctx, "End metadata of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	tflog.Debug(ctx, "Start schema of datasource: aci_certificate_authority")
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "The certificate_authority datasource for the 'pkiTP' class",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "The distinguished name (DN) of the Certificate Authority object.",
+			},
+			"parent_dn": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "The distinguished name (DN) of the parent object.",
+			},
+			"annotation": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The annotation of the Certificate Authority object.`,
+			},
+			"certificate_chain": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The PEM-encoded chain of trust from the trustpoint to a trusted root authority.`,
+			},
+			"description": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The description of the Certificate Authority object.`,
+			},
+			"name": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: `The name of the Certificate Authority object.`,
+			},
+			"name_alias": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The name alias of the Certificate Authority object.`,
+			},
+			"owner_key": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `The key for enabling clients to own their data for entity correlation.`,
+			},
+			"owner_tag": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: `A tag for enabling clients to add their own data. For example, to indicate who created this object.`,
+			},
+			"annotations": schema.SetNestedAttribute{
+				MarkdownDescription: ``,
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"key": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The key used to uniquely identify this configuration object.`,
+						},
+						"value": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The value of the property.`,
+						},
+					},
+				},
+			},
+			"tags": schema.SetNestedAttribute{
+				MarkdownDescription: ``,
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"key": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The key used to uniquely identify this configuration object.`,
+						},
+						"value": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: `The value of the property.`,
+						},
+					},
+				},
+			},
+		},
+	}
+	tflog.Debug(ctx, "End schema of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	tflog.Debug(ctx, "Start configure of datasource: aci_certificate_authority")
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *client.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+	tflog.Debug(ctx, "End configure of datasource: aci_certificate_authority")
+}
+
+func (d *PkiTPDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	tflog.Debug(ctx, "Start read of datasource: aci_certificate_authority")
+	var data *PkiTPResourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if data.ParentDn.IsNull() || data.ParentDn.IsUnknown() {
+		data.ParentDn = basetypes.NewStringValue("uni/userext/pkiext")
+	}
+
+	setPkiTPId(ctx, data)
+
+	// Create a copy of the Id for when not found during getAndSetPkiTPAttributes
+	cachedId := data.Id.ValueString()
+
+	tflog.Debug(ctx, fmt.Sprintf("Read of datasource aci_certificate_authority with id '%s'", data.Id.ValueString()))
+
+	getAndSetPkiTPAttributes(ctx, &resp.Diagnostics, d.client, data)
+
+	if data.Id.IsNull() {
+		resp.Diagnostics.AddError(
+			"Failed to read aci_certificate_authority data source",
+			fmt.Sprintf("The aci_certificate_authority data source with id '%s' has not been found", cachedId),
+		)
+		return
+	}
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	tflog.Debug(ctx, fmt.Sprintf("End read of datasource aci_certificate_authority with id '%s'", data.Id.ValueString()))
+}